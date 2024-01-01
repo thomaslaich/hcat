fb4650765788c6770b7554a2320d1c93ed7f2db1
Thomas Laich <thomaslaich@gmail.com>
2024-01-01 18:54:07 +0100

    First somewhat working implementation

--- src/HCat.hs
+++ src/HCat.hs
@@ -4,10 +4,82 @@ import Control.Exception qualified as Exception
 import Data.ByteString qualified as BS
 import Data.Text qualified as Text
 import Data.Text.IO qualified as TextIO
+import Data.Time.Clock qualified as Clock
+import Data.Time.Clock.POSIX qualified as PosixClock
+import Data.Time.Format qualified as TimeFormat
+import System.Directory qualified as Directory
 import System.Environment qualified as Env
+import System.IO
 import System.IO.Error qualified as IOError
 import System.Info qualified as SystemInfo
 import System.Process (readProcess)
+import Text.Printf
+
+data ContinueCancel = Continue | Cancel deriving (Eq, Show)
+
+data FileInfo = FileInfo
+  { filePath :: FilePath
+  , fileSize :: Int
+  , fileMTime :: Clock.UTCTime
+  , fileReadable :: Bool
+  , fileWritable :: Bool
+  , fileExecutable :: Bool
+  }
+  deriving (Show)
+
+data ScreenDimensions = ScreenDimensions
+  { screenRows :: Int
+  , screenColumns :: Int
+  }
+  deriving (Show)
+
+getFileInfo :: FilePath -> IO FileInfo
+getFileInfo filePath = do
+  perms <- Directory.getPermissions filePath
+  mtime <- Directory.getModificationTime filePath
+  size <- BS.length <$> BS.readFile filePath
+  return $
+    FileInfo
+      { filePath = filePath
+      , fileSize = size
+      , fileMTime = mtime
+      , fileReadable = Directory.readable perms
+      , fileWritable = Directory.writable perms
+      , fileExecutable = Directory.executable perms
+      }
+
+formatFileInfo :: FileInfo -> Int -> Int -> Int -> Text.Text
+formatFileInfo FileInfo {..} maxWidth totalPages currentPage =
+  let
+    timestamp = TimeFormat.formatTime TimeFormat.defaultTimeLocale "%F %T" fileMTime
+    permissionString =
+      [ if fileReadable then 'r' else '-'
+      , if fileWritable then 'w' else '-'
+      , if fileExecutable then 'x' else '-'
+      ]
+    statusLine =
+      Text.pack $
+        printf
+          "%s | permissions: %s | %d bytes | modified: %s | page %d of %d"
+          filePath
+          permissionString
+          fileSize
+          timestamp
+          currentPage
+          totalPages
+   in
+    invertText (truncateStatus statusLine)
+  where
+    invertText inputStr =
+      let
+        reverseVideo = "\^[[7m"
+        resetVideo = "\^[[0m"
+       in
+        reverseVideo <> inputStr <> resetVideo
+    truncateStatus statusLine
+      | maxWidth <= 3 = ""
+      | Text.length statusLine > maxWidth = Text.take (maxWidth - 3) statusLine <> "..."
+      | otherwise = statusLine
 
 getTerminalSize :: IO ScreenDimensions
 getTerminalSize =
@@ -25,6 +97,16 @@ getTerminalSize =
               let lines' = read $ init lines
                   cols' = read $ init cols
                in return $ ScreenDimensions lines' cols'
+
+getContinue :: IO ContinueCancel
+getContinue = do
+  hSetBuffering stdin NoBuffering
+  hSetEcho stdin False
+  input <- hGetChar stdin
+  case input of
+    ' ' -> return Continue
+    'q' -> return Cancel
+    _ -> getContinue
 
 handleArgs :: IO (Either String FilePath)
 handleArgs =
@@ -63,29 +145,40 @@ wordWrap lineLength lineText
            in (wrappedLine, Text.tail rest)
       | otherwise = softWrap hardwrappedText (textIndex - 1)
 
-data ScreenDimensions = ScreenDimensions
-  { screenRows :: Int
-  , screenColumns :: Int
-  }
-  deriving (Show)
-
-paginate :: ScreenDimensions -> Text.Text -> [Text.Text]
-paginate (ScreenDimensions rows columns) text =
+paginate :: ScreenDimensions -> FileInfo -> Text.Text -> [Text.Text]
+paginate (ScreenDimensions rows columns) finfo text =
   let
-    unwrappedLines = Text.lines text
-    wrappedLines = concatMap (wordWrap columns) unwrappedLines
-    paginatedLines = groupsOf rows wrappedLines
+    rows' = rows - 1
+    wrappedLines = concatMap (wordWrap columns) (Text.lines text)
+    pages = map (Text.unlines . padTo rows') $ groupsOf rows' wrappedLines
+    pageCount = length pages
+    statusLines = map (formatFileInfo finfo columns pageCount) [1 .. pageCount]
    in
-    map Text.unlines paginatedLines
+    zipWith (<>) pages statusLines
+  where
+    padTo :: Int -> [Text.Text] -> [Text.Text]
+    padTo lineCount rowsToPad =
+      take lineCount $ rowsToPad <> repeat ""
+
+showPages :: [Text.Text] -> IO ()
+showPages [] = return ()
+showPages (page : pages) = do
+  clearScreen
+  TextIO.putStrLn page
+  input <- getContinue
+  case input of
+    Continue -> showPages pages
+    Cancel -> return ()
+
+clearScreen :: IO ()
+clearScreen = BS.putStr "\^[[1J\^[[1;1H"
 
 runHCat :: IO ()
-runHCat =
-  withErrorHandling $
-    handleArgs
-      >>= eitherToErr
-      >>= TextIO.readFile
-      >>= TextIO.putStrLn
-  where
-    withErrorHandling ioAction =
-      Exception.catch ioAction $
-        \err -> putStrLn "I ran into an error: " >> print @IOError.IOError err
+runHCat = do
+  targetFilePath <- eitherToErr =<< handleArgs
+  contents <- TextIO.hGetContents =<< openFile targetFilePath ReadMode
+  termSize <- getTerminalSize
+  hSetBuffering stdin NoBuffering
+  finfo <- getFileInfo targetFilePath
+  let pages = paginate termSize finfo contents
+  showPages pages
